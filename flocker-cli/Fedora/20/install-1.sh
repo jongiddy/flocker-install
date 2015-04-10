5b65dc5f489b23130b63a051e3caa09377d63d97
Jonathan Giddy <jon.giddy@clusterhq.com>
2015-04-10 10:16:23 +0100

    Add CLI installation scripts

---
+++ flocker-cli/Fedora/20/install-1.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e -x
+
+# Add ClusterHQ repository
+sudo yum install -y https://s3.amazonaws.com/clusterhq-archive/fedora/clusterhq-release$(rpm -E %dist).noarch.rpm
+
+# Add ClusterHQ packages
+sudo yum -y install clusterhq-python-flocker clusterhq-flocker-cli
+
+echo "Flocker CLI ${version} installed."
---
+++ flocker-cli/Ubuntu/14.04/install-1.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e -x
+
+# Add ClusterHQ repository
+sudo add-apt-repository -y 'deb http://build.clusterhq.com/results/omnibus/master/ubuntu-14.04 /'
+sudo apt-get update
+
+# Unauthenticated packages need --force-yes
+sudo apt-get -y --force-yes install clusterhq-python-flocker clusterhq-flocker-cli
+
+echo "Flocker CLI ${version} installed."
