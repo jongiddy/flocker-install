7aac271225b9a6c920aa3bf74b6457a718a317ff
Jonathan Giddy <jon.giddy@clusterhq.com>
2015-04-10 15:53:34 +0100

    Add script to run CLI in Vagrant

---
+++ flocker-cli/CentOS/7.0/vagrant.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e -x
+
+tmpdir=/tmp/flocker.$$
+
+mkdir ${tmpdir}
+
+cp install-1.sh ${tmpdir}/install-1.sh
+
+cat > ${tmpdir}/Vagrantfile <<EOF
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "chef/centos-7.0"
+  config.vm.provision :shell, :path => "install-1.sh"
+end
+EOF
+
+cd ${tmpdir}
+
+vagrant up
+
+vagrant ssh
+
+vagrant destroy --force
+
+cd -
+
+rm -fr ${tmpdir}
