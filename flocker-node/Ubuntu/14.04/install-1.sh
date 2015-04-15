e03cc425d7fdf981cbe8ed46cbca1097080bcfc4
Jonathan Giddy <jon.giddy@clusterhq.com>
2015-04-15 11:48:14 +0100

    Install libc6-dev on Ubuntu to prevent kernel module failure
    Package spl-dkms sometimes does not have libc6-dev as a
    dependency, add it before ZFS installation requires it.

--- flocker-node/Ubuntu/14.04/install-1.sh
+++ flocker-node/Ubuntu/14.04/install-1.sh
@@ -3,13 +3,27 @@
 set -e -x
 
 # Add ClusterHQ and recent ZFS and Docker repositories
+
+# Ensure add-apt-repository command is available
 sudo apt-get -y install software-properties-common
+
+# ZFS not available in base Ubuntu - add ZFS repo
 sudo add-apt-repository -y ppa:zfs-native/stable
+
+# Add Docker repo for recent Docker versions
 sudo add-apt-repository -y ppa:james-page/docker
+
+# Add ClusterHQ repo for installation of Flocker packages.
 sudo add-apt-repository -y 'deb http://build.clusterhq.com/results/omnibus/master/ubuntu-14.04 /'
+
+# Update to read package info from new repos
 sudo apt-get update
 
-# Add ClusterHQ packages
+# Package spl-dkms sometimes does not have libc6-dev as a
+# dependency, add it before ZFS installation requires it.
+sudo apt-get -y install libc6-dev
+
+# Install Flocker node and all dependencies
 # Unauthenticated packages need --force-yes
 sudo apt-get -y --force-yes install clusterhq-flocker-node
 
