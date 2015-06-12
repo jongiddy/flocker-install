53077bd157294bd9be2623671609b487d2a5f8ea
Jonathan Giddy <jon.giddy@clusterhq.com>
2015-06-12 18:30:57 +0100

    Update test to set number of nodes variable

--- run-test.sh
+++ run-test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+source flocker-client/bin/activate
+
 export FLOCKER_ACCEPTANCE_NODES=$(cat control.txt agents.txt | sort | uniq | paste -s -d: -)
+export FLOCKER_ACCEPTANCE_NUM_AGENT_NODES=$(cat control.txt agents.txt | sort | uniq | wc -l)
 export FLOCKER_ACCEPTANCE_CONTROL_NODE=$(cat control.txt)
 export FLOCKER_ACCEPTANCE_AGENT_NODES=$(cat agents.txt | paste -s -d: -)
 export FLOCKER_ACCEPTANCE_VOLUME_BACKEND=zfs
