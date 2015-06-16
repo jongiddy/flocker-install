c24e5e533c56f76b4cf2fbec86d7726ae65e8105
Jonathan Giddy <jon.giddy@clusterhq.com>
2015-06-16 09:50:51 +0100

    Only count agent nodes for acceptance tests environment variable

--- run-test.sh
+++ run-test.sh
@@ -2,7 +2,7 @@
 
 source flocker-client/bin/activate
 
-export FLOCKER_ACCEPTANCE_NUM_AGENT_NODES=$(cat control.txt agents.txt | sort | uniq | wc -l)
+export FLOCKER_ACCEPTANCE_NUM_AGENT_NODES=$(cat agents.txt | wc -l)
 export FLOCKER_ACCEPTANCE_CONTROL_NODE=$(cat control.txt)
 export FLOCKER_ACCEPTANCE_VOLUME_BACKEND=zfs
 export FLOCKER_ACCEPTANCE_API_CERTIFICATES_PATH=`pwd`
